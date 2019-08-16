70b21268fdeb5e3dcc0680312ecd9633c0fef2bd
hj37 <54181059+hj37@users.noreply.github.com>
2019-08-17 00:16:11 +0900

    초기 프로젝트 설정

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterBehavior : MonoBehaviour {    //캐릭터의 행동을 정의하는 스크립트 
+
+    private CharacterStat characterStat;
+    private GameManager gameManager;
+
+    public GameObject bullet;
+    private Animator animator;
+    private AudioSource audioSource;
+    
+	void Start () {
+        characterStat = gameObject.GetComponent<CharacterStat>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        animator = gameObject.GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void attack(int damage)
+    {
+        animator.SetTrigger("Attack");
+        audioSource.PlayOneShot(audioSource.clip);
+        GameObject currentBullet = Instantiate(bullet, transform.position, Quaternion.identity);    //총알 객체 만든부분 
+        currentBullet.GetComponent<BulletBehavior>().setDamage(damage);
+    }
+	
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject(-1) == true) return;
+        if (EventSystem.current.IsPointerOverGameObject(0) == true) return;
+        if (characterStat.canLevelUp(gameManager.seed))
+        {
+            characterStat.increaseLevel();
+            gameManager.seed -= characterStat.upgradeCost;
+            gameManager.updateText();
+        }
+    }
+}
